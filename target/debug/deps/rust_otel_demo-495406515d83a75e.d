372d9da5223021be937bc3f9d24a0a286ce5d11a
John Sigvald Skauge <john.sigvald.skauge@dnb.no>
2024-10-17 14:03:54 +0200

    Working otel stack

--- target/debug/deps/rust_otel_demo-495406515d83a75e.d
+++
@@ -1,5 +0,0 @@
-/Users/john.sigvald.skauge/Projects/rust-otel-demo/target/debug/deps/librust_otel_demo-495406515d83a75e.rmeta: src/main.rs
-
-/Users/john.sigvald.skauge/Projects/rust-otel-demo/target/debug/deps/rust_otel_demo-495406515d83a75e.d: src/main.rs
-
-src/main.rs:
