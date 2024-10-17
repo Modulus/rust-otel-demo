b9d282df769d59d2494552d555bcff455938428d
John Sigvald Skauge <john.sigvald.skauge@dnb.no>
2024-10-17 14:03:02 +0200

    Initial commit and working otel stach

---
+++ target/debug/deps/rust_otel_demo-af654a250a0a1304.d
@@ -0,0 +1,5 @@
+/Users/john.sigvald.skauge/Projects/rust-otel-demo/target/debug/deps/librust_otel_demo-af654a250a0a1304.rmeta: src/main.rs
+
+/Users/john.sigvald.skauge/Projects/rust-otel-demo/target/debug/deps/rust_otel_demo-af654a250a0a1304.d: src/main.rs
+
+src/main.rs:
